2bf06f19a279d09280e25e90c44f489d3e254524
Dragan Cecavac <dragan.cecavac@mips.com>
2017-10-18 17:40:50 +0200

    Enable VtsKernelLinuxKselftest and VtsKernelLinuxKselftestPresubmit modules for MIPS
    Change-Id: Ie3f4f8c02d17af30d17b2940cce8595c1ea3dbd8
    Signed-off-by: Dragan Cecavac <dragan.cecavac@mips.com>

--- linux_kselftest/kselftest_config.py
+++ linux_kselftest/kselftest_config.py
@@ -32,33 +32,33 @@ KSFT_DIR = "/data/local/tmp/linux-kselftest"
 
 # Presubmit, stable, and staging lists are always mutually exclusive.
 KSFT_CASES_PRESUBMIT = map(lambda x: test_case.LinuxKselftestTestcase(*(x)), [
-    ("futex/functional/futex_wait_timeout", ["arm", "x86"], [32, 64]),
-    ("futex/functional/futex_wait_wouldblock", ["arm", "x86"], [32, 64]),
-    ("futex/functional/futex_requeue_pi_mismatched_ops", ["arm", "x86"], [32, 64]),
-    ("futex/functional/futex_wait_uninitialized_heap", ["arm", "x86"], [32]),
-    ("futex/functional/futex_wait_private_mapped_file", ["arm", "x86"], [32, 64]),
-    ("net/socket", ["arm", "x86"], [32, 64]),
+    ("futex/functional/futex_wait_timeout", ["arm", "mips", "x86"], [32, 64]),
+    ("futex/functional/futex_wait_wouldblock", ["arm", "mips", "x86"], [32, 64]),
+    ("futex/functional/futex_requeue_pi_mismatched_ops", ["arm", "mips", "x86"], [32, 64]),
+    ("futex/functional/futex_wait_uninitialized_heap", ["arm", "mips", "x86"], [32]),
+    ("futex/functional/futex_wait_private_mapped_file", ["arm", "mips", "x86"], [32, 64]),
+    ("net/socket", ["arm", "mips", "x86"], [32, 64]),
 ])
 
 KSFT_CASES_STABLE = map(lambda x: test_case.LinuxKselftestTestcase(*(x)), [
-    ("net/psock_tpacket", ["arm", "x86"], [32, 64]),
-    ("ptrace/peeksiginfo", ["arm", "x86"], [64]),
-    ("timers/posix_timers", ["arm", "x86"], [32, 64]),
-    ("timers/nanosleep", ["arm", "x86"], [32, 64]),
-    ("timers/nsleep-lat", ["arm", "x86"], [32, 64]),
-    ("timers/set-timer-lat", ["arm", "x86"], [32, 64]),
-    ("timers/inconsistency-check", ["arm", "x86"], [32, 64]),
-    ("timers/raw_skew", ["arm", "x86"], [32, 64]),
-    ("timers/threadtest", ["arm", "x86"], [32, 64]),
-    ("timers/set-tai", ["arm", "x86"], [32, 64]),
-    ("timers/valid-adjtimex", ["arm", "x86"], [64]),
+    ("net/psock_tpacket", ["arm", "mips", "x86"], [32, 64]),
+    ("ptrace/peeksiginfo", ["arm", "mips", "x86"], [64]),
+    ("timers/posix_timers", ["arm", "mips", "x86"], [32, 64]),
+    ("timers/nanosleep", ["arm", "mips", "x86"], [32, 64]),
+    ("timers/nsleep-lat", ["arm", "mips", "x86"], [32, 64]),
+    ("timers/set-timer-lat", ["arm", "mips", "x86"], [32, 64]),
+    ("timers/inconsistency-check", ["arm", "mips", "x86"], [32, 64]),
+    ("timers/raw_skew", ["arm", "mips", "x86"], [32, 64]),
+    ("timers/threadtest", ["arm", "mips", "x86"], [32, 64]),
+    ("timers/set-tai", ["arm", "mips", "x86"], [32, 64]),
+    ("timers/valid-adjtimex", ["arm", "mips", "x86"], [64]),
 ])
 
 KSFT_CASES_STAGING = map(lambda x: test_case.LinuxKselftestTestcase(*(x)), [
 # TODO(trong): enable pstore test.
-#    ("pstore/pstore_tests", ["arm", "x86"], [32, 64]),
-    ("seccomp/seccomp_bpf", ["arm", "x86"], [32, 64]),
-    ("timers/alarmtimer-suspend", ["arm", "x86"], [32, 64]),
+#    ("pstore/pstore_tests", ["arm", "mips", "x86"], [32, 64]),
+    ("seccomp/seccomp_bpf", ["arm", "mips", "x86"], [32, 64]),
+    ("timers/alarmtimer-suspend", ["arm", "mips", "x86"], [32, 64]),
     ("x86/single_step_syscall", ["x86"], [32, 64]),
     ("x86/sysret_ss_attrs", ["x86"], [32]),
     ("x86/syscall_nt", ["x86"], [32, 64]),
